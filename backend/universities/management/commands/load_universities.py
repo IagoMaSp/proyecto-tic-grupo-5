2f7e39177a26880e21ffb1f33282c6d8d909e593
paulabachmann <bachmannpaula26@gmail.com>
2025-11-05 10:45:03 -0300

    cambio continentes para que machee bien

--- backend/universities/management/commands/load_universities.py
+++ backend/universities/management/commands/load_universities.py
@@ -171,7 +171,7 @@ class Command(BaseCommand):
     def map_continent(self, continent_qs):
         mapping ={
             'Americas': 'America',
-            'Europe': 'Europa',
+            'Europe': 'Europe',
             'Asia': 'Asia',
             'Oceania': 'Oceania',
             'Africa': 'Africa'
